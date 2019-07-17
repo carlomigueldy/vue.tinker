88d1925267a95a7977da64b1bb9427eed3b34c40
Carlo Miguel Dy <carlomigueldy@gmail.com>
2019-07-17 20:47:17 +0800

    Progress on debuggin

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,32 +1,77 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+import Articles from './components/Articles';
 
 const app = new Vue({
     el: '#app',
+
+    components: { Articles },
+
+    mounted() {
+        this.initMap();
+        this.leafletDraw();
+    },
+
+    methods: {
+        leafletDraw() {
+            const drawnItems = new L.FeatureGroup();
+            this.map.addLayer(drawnItems);
+
+            const drawControl = new L.Control.Draw({
+                draw: {
+                    circle: false,
+                    circlemarker: false,
+                },
+                edit: {
+                    featureGroup: drawnItems,
+                    edit: false,
+                }
+            });
+            this.map.addControl(drawControl);
+
+            this.map.on('draw:created', function (e) {
+                const type = e.layerType;
+                const layer = e.layer;
+
+                // console.log("Check value for drawnItems => ");
+                // console.log(drawnItems);
+                // console.log(layer);
+                // console.log(type);
+                // console.log("Check value for layer => ");
+                // console.log(this.layer);
+
+                drawnItems.addLayer(layer);
+
+                if(type == 'polygon' || type == 'rectangle') {
+                    const coordinates = (JSON.stringify(layer.getLatLngs().map(function(point) {
+                            console.log(`${point[i].lat}, ${point[i].lat}`);
+                            return ([point.lat, point.lng]);  
+                        })
+                    ));
+                }
+
+                // if (this.type == 'marker') {
+                //     this.lat = layer.getLatLng().lat;
+                //     this.lng = layer.getLatLng().lng;
+                //     console.log("Latitude: " + this.lat + " | Longitude: " + this.lng);
+                // }
+            });
+        },
+
+        initMap() {
+            this.map = L.map('map').setView([-41.2858, 174.78682], 14);
+
+            this.tileLayer = L.tileLayer(
+            'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+            {
+                maxZoom: 18,
+                attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+            }
+            );
+            this.tileLayer.addTo(this.map);
+        },
+
+    },
 });
---
+++ resources/js/components/Articles.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h2>Articles</h2>
+        <div id="map"></div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted, coming from the Articles component.')
+        },
+    }
+</script>
+
+<style>
+#map { 
+    height: 300px;
+}
+</style>
